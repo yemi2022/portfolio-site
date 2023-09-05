9275e628e3bea8a54ea9e4f7b6dbf9a9fb7b0c56
Obidare Opeyemi <obidareopeyemi@gmail.com>
2023-09-05 21:20:35 +0100

    new

--- src/App.js
+++ src/App.js
@@ -192,10 +192,10 @@ function App() {
       <p className='text-center mb-4'>Below are some of my completed projects</p>
       <div className='mx-auto bg-blue-700 w-12 h-2 mb-12'></div>
       <div id='project-group' className='flex flex-wrap justify-center items-stretch gap-8'>
-        <div id='projectbox' className='w-[25em] group bg-neutral-200'>
+        <div id='projectbox' className='w-[22em] group bg-neutral-200'>
           <a href='#home' className=''>
             <div className="relative overflow-hidden w-full">
-              <img src={portfolio} alt="project" className="w-full h-auto group-hover:scale-130 ease-in transition-transform duration-500" />
+              <img src={portfolio} alt="project" className="w-full h-auto group-hover:scale-130 sm:group-hover:scale-100 ease-in transition-transform duration-500" />
             </div>
             </a>
           <div id='project-info' className='px-3 pb-4 pt-8'>
@@ -208,10 +208,10 @@ function App() {
           </div>
         </div>
         
-        <div id='projectbox' className='w-[25em] group bg-neutral-200'>
+        <div id='projectbox' className='w-[22em] group bg-neutral-200'>
           <a href='https://job.codecrafted.com.ng' target='_blank' rel='noreferrer' className=''>
             <div className="relative overflow-hidden w-full">
-              <img src={pic} alt="project" className="w-full h-auto group-hover:scale-130 ease-in transition-transform duration-500" />
+              <img src={pic} alt="project" className="w-full h-auto group-hover:scale-130 sm:group-hover:scale-100 ease-in transition-transform duration-500" />
             </div>
             </a>
           <div id='project-info' className='px-3 pt-8 pb-4'>
@@ -224,10 +224,10 @@ function App() {
           </div>
         </div>
         
-        <div id='projectbox' className='w-[25em] group bg-neutral-200'>
+        <div id='projectbox' className='w-[22em] group bg-neutral-200'>
           <a href='https://weather.codecrafted.com.ng' target='_blank' rel='noreferrer' className=''>
             <div className="relative overflow-hidden w-full">
-              <img src={weather} alt="project" className="w-full h-auto group-hover:scale-130 ease-in transition-transform duration-500" />
+              <img src={weather} alt="project" className="w-full h-auto group-hover:scale-130 sm:group-hover:scale-100 ease-in transition-transform duration-500" />
             </div>
             </a>
           <div id='project-info' className='px-3 pt-8 pb-4'>
@@ -235,15 +235,15 @@ function App() {
             <p className='text-sm mb-2'><span className='text-blue-500'>Vue</span> / August, 2023</p>
             <p className='text-sm mb-2'>Vue app that uses open weather API to retrieve and display the current weather of searched locations.</p>
             <p className='text-sm mb-2'>This site is best viewed on PC.</p>
-            <a href='https://github.com/yemi2022/cmc-html-and-css.git' target='_blank' rel='noreferrer' className='text-sm text-blue-700 mb-2'>See the code on Github</a><br />
+            <a href='https://github.com/yemi2022/weather-api.git' target='_blank' rel='noreferrer' className='text-sm text-blue-700 mb-2'>See the code on Github</a><br />
             <a href='https://weather.codecrafted.com.ng' target='_blank' rel='noreferrer' className='text-sm text-blue-700'>Visit the Site&#127993;</a>
           </div>
         </div>
         
-        <div id='projectbox' className='w-[25em] group bg-neutral-200'>
+        <div id='projectbox' className='w-[22em] group bg-neutral-200'>
           <a href='https://cmc.codecrafted.com.ng' target='_blank' rel='noreferrer' className=''>
             <div className="relative overflow-hidden w-full">
-              <img src={pic2} alt="project" className="w-full h-auto group-hover:scale-130 ease-in transition-transform duration-500" />
+              <img src={pic2} alt="project" className="w-full h-auto group-hover:scale-130 sm:group-hover:scale-100 ease-in transition-transform duration-500" />
             </div>
             </a>
           <div id='project-info' className='px-3 pt-8 pb-4'>
@@ -251,15 +251,15 @@ function App() {
             <p className='text-sm mb-2'><span className='text-blue-500'>Vue and Tailwind CSS</span> / July, 2023</p>
             <p className='text-sm mb-2'>Cartoon site rebuilt with Vue and Tailwind CSS with light and dark depending on the device's active theme.</p>
             <p className='text-sm mb-2'>This site is best viewed on PC and mobile portrait mode.</p>
-            <a href='https://github.com/yemi2022/cmc-html-and-css.git' target='_blank' rel='noreferrer' className='text-sm text-blue-700 mb-2'>See the code on Github</a><br />
+            <a href='https://github.com/yemi2022/vue-cartoon-site.git' target='_blank' rel='noreferrer' className='text-sm text-blue-700 mb-2'>See the code on Github</a><br />
             <a href='https://cmc.codecrafted.com.ng' target='_blank' rel='noreferrer' className='text-sm text-blue-700'>Visit the Site&#127993;</a>
           </div>
         </div>
         
-        <div id='projectbox' className='w-[25em] group bg-neutral-200'>
+        <div id='projectbox' className='w-[22em] group bg-neutral-200'>
           <a href='https://cartoon.codecrafted.com.ng' target='_blank' rel='noreferrer' className=''>
             <div className="relative overflow-hidden w-full">
-              <img src={pic} alt="project" className="w-full h-auto group-hover:scale-130 ease-in transition-transform duration-500" />
+              <img src={pic} alt="project" className="w-full h-auto group-hover:scale-130 sm:group-hover:scale-100 ease-in transition-transform duration-500" />
             </div>
             </a>
           <div id='project-info' className='px-3 pt-8 pb-4'>
@@ -275,12 +275,60 @@ function App() {
     
       </div>
   </section>
-  <section id="contact" className='p-14 sm:px-4'>
-      <h1>CONTACT SECTION</h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem architecto a at repudiandae similique exercitationem laudantium iste fugiat? Sapiente consectetur necessitatibus debitis cumque, molestiae tempore non dolor quibusdam nesciunt quidem possimus deserunt ea aut expedita eaque distinctio, maxime natus. Sint nihil ab corrupti voluptatum, architecto corporis quibusdam at veritatis nostrum temporibus ullam iusto res perferendis! Eos, sapiente! Delectus illum laudantium architecto error qui nesciunt eaque obcaecati, ea ipsa reprehenderit explicabo, iure reiciendis adipisci autem modi, vel repudiandae minus ullam? Sint, consequatur fuga consequuntur, distinctio ea ratione natus labore, vel aut nemo quae unde fugit cumque atque veritatis amet molestiae quia expedita asperiores suscipit maiores maxime ipsam et qui. In at architecto ea distinctio amet totam maiores earum minus officiis fuga cupiditate minima quidem esse debitis facilis assumenda eligendi et, blanditiis veniam. Eum placeat soluta natus eveniet excepturi?
+  <section className='w-[98.7vw] flex justify-center py-[8.5vh] -ml-20 sm:-ml-3 sm:portrait:py-[] sm:landscape:py-[14.9vh] sm:landscape:-ml-5 sm:w-[100vw] h-max bg-[#1d4ed8cc]'>
+    <div id="contact" className='bg-white w-[80%] h-max p-14 sm:portrait:p-4  sm:portrait:w-[90%] flex flex-wrap gap-6 justify-center'>
+      <div className='w-[45%] sm:portrait:w-full'>
+        <h1 className='font-semibold text-2xl mb-1'>Send me a message</h1>
+        <div className='bg-blue-700 w-32 h-1 mb-4'></div>
+        <form action='mailto:obidareopeyemi@gmail.com' method='get' encType='text/plain' >
+          <input type="text" name="subject" className='border text-sm mb-4 w-full p-2 placeholder-gray-500 border-gray-300 focus:outline-blue-500' placeholder="Your Name" minLength={3} maxLength={30} required /><br/>
+          <input type="email" name="email" placeholder="Your Email" className='border text-sm mb-4 w-full p-2 placeholder-gray-500 border-gray-300 focus:outline-blue-500' required /><br/>
+          <textarea name="body" rows="5" cols="30" placeholder="Your Message" className='border text-sm mb-4 w-full p-2 placeholder-gray-500 border-gray-300 focus:outline-blue-500'></textarea><br/>
+          <input type="submit" value="Send" className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center' />
+        </form>
+      </div>
+      <div className='w-[45%] sm:portrait:w-full sm:portrait:mb-16'>
+        <h1 className='font-semibold text-2xl mb-1'>Get in touch</h1>
+        <div className='bg-blue-700 w-32 h-1 mb-4'></div>
+        <p className='mb-6'>You can reach me on any of the following social media.</p>
+        <div className='flex justify-center gap-4'>
+          <a href='https://wa.link/bc73co' target='_blank' rel='noreferrer'>
+            <svg
+            class="w-6 h-6 text-green-400 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512">
+              <path
+                d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
+              ></path>
+            </svg>
+          </a>
+          <a href='https://www.linkedin.com/in/opeyemi-obidare-371715193/' target='_blank' rel='noreferrer'>
+            <svg
+            class="w-6 h-6 text-blue-500 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512">
+              <path
+                d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+              ></path>
+            </svg>
+          </a>
+          <a href='https://twitter.com/ope_obidare' target='_blank' rel='noreferrer'>
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-black fill-current" viewBox="0 0 512 512">
+          
+          <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+          </a>
+        </div>
+        
+      </div>
+    </div>
   </section>
 
 </main>
+<footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2023 Obidare Opeyemi Emmanuel</p>
+      </div>
+    </footer>
     </div>
   );
 }
